d8dd1e61a31a4c0cc519cbe642d10862eac3918c
abubakar471 <abdurjoy2001@gmail.com>
2023-10-05 02:37:40 +0600

    changed env urls

--- node-server/routes/guess.js
+++ node-server/routes/guess.js
@@ -4,7 +4,7 @@ const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 router.post("/game-start", async (req, res) => {
-  const response = await fetch("http://127.0.0.1:5000/game-start", {
+  const response = await fetch("http://abdevs4371.pythonanywhere.com/game-start", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   });
--- node-server/server.js
+++ node-server/server.js
@@ -9,23 +9,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  origin : process.env.CLIENT_URL
+}));
 
 app.use("/api/guess", guess);
-
-// app.get("/", async (req, res) => {
-//   // this var contains the data you want to send
-//   var data = [2, 8, 4, 5, 6];
-//   const response = await fetch("http://127.0.0.1:5000/arraysum", {
-//     method: "POST",
-//     body: JSON.stringify({array : data}),
-//     headers: { "Content-Type": "application/json" },
-//   });
-//   const result = await response.json()
-//   console.log(result);
-// });
-
-
 
 const port = process.env.PORT || 8000;
 
